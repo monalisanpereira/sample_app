cb8aef2d58d2ba3ee9eb87d66bac1b9d9cf3bc6b
Monalisa Pereira <monalisanpereira@gmail.com>
2022-10-15 15:06:09 +0000

    Add follow feature

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -15,6 +15,9 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'h1>img.gravatar'
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination', count: 1
+    assert_select 'div.stats', count: 1
+    assert_select '#following', @user.following.count.to_s
+    assert_select '#followers', @user.followers.count.to_s
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
